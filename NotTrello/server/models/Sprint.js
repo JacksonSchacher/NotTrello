7d74c6c3fb2997a23655833e44bdc84c24ad870e
Jackson Schacher <jackson.schacher@gmail.com>
2021-09-27 15:00:15 -0600

    update

---
+++ NotTrello/server/models/Sprint.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+export const SprintSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
+    projectId: { type: Schema.Types.ObjectId, ref: 'Project', required: true },
+    creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true },
+    isOpen: { type: Boolean }
+  }
+)
+SprintSchema.virtual('project', {
+  localField: 'projectId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Project'
+})
+
+SprintSchema.virtual('creator', {
+  localField: 'creatorId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Account'
+})
