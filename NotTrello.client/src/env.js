6f9ce5e5d2d3219930d6cbc7ecfefbecc09552bb
Jackson Schacher <jackson.schacher@gmail.com>
2021-09-27 14:30:49 -0600

    updated env

--- NotTrello.client/src/env.js
+++ NotTrello.client/src/env.js
@@ -2,5 +2,5 @@ export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
 export const domain = 'sprintr-user.us.auth0.com'
-export const clientId = 'pwfiMIJslkgu1IQJXg8FgC5kwxcZrcZQ'
-export const audience = 'https://sprintr-user.us.auth0.com/api/v2/'
+export const clientId = '3AaXEjj2MMaWxbvfSRM2vbBzTbQT6JwD'
+export const audience = 'https://sprintr.com'
