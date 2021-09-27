260f4f5b1bdf668b8187f5d497c1ba266b3e2a15
Hannah Milam <hannah.milam16@gmail.com>
2021-09-27 15:01:45 -0600

    updated schemas

---
+++ NotTrello/server/models/Task.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+export const TaskSchema = new Schema ({
+  name: {type: String, required: true},
+  weight: {type: Number, required: true},
+  projectId: { type: Schema.Types.ObjectId, required: true, ref: 'Project' },
+  backlogItemId: { type: Schema.Types.ObjectId, required: true, ref: 'BacklogItem' },
+  creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Account' },
+  completedOn: {type: Date},
+  assignedTo: { type: Schema.Types.ObjectId, required: true, ref: 'Profile' },
+},
+{ timestamps: true, toJSON: { virtuals: true }} 
+)
+
+TaskSchema.virtual('backlogItem', {
+  localField: 'backlogItemId',
+  foreignField: '_id',
+  justOne: true, 
+  ref: 'BacklogItem'
+})
+TaskSchema.virtual('project', {
+  localField: 'projectId',
+  foreignField: '_id',
+  justOne: true, 
+  ref: 'Project'
+})
+TaskSchema.virtual('creator', {
+  localField: 'creatorId',
+  foreignField: '_id',
+  justOne: true, 
+  ref: 'Account'
+})
+TaskSchema.virtual('assignedTo', {
+  localField: 'assignedToId',
+  foreignField: '_id',
+  justOne: true, 
+  ref: 'Profile'
+})
