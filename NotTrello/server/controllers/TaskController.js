cda0ba9590ac7efbab7766f50097b30fe00a9f45
Hannah Milam <hannah.milam16@gmail.com>
2021-09-27 23:24:50 -0600

    hopefully this works

--- NotTrello/server/controllers/TaskController.js
+++ NotTrello/server/controllers/TaskController.js
@@ -59,48 +59,5 @@ export class TaskController extends BaseController {
       next(error)
     }
   }
-  async getTasks(req, res, next) {
-    try {
-      const task = await tasksService.getTasks({ projectId: req.params.projectId })
-      res.send(task)
-    } catch (error) {
-      next(error)
-    }
-  }
-  async getTaskById(req, res, next) {
-    try {
-      const task = await tasksService.getTaskById(req.params.taskId)
-      res.send(task)
-    } catch (error) {
-      next(error)
-    }
-  }
-  async createTask(req, res, next){
-    try {
-      req.body.creatorId = req.userInfo.id
-      const task = await tasksService.createTask(req.body)
-      res.send(task)
-    } catch (error) {
-      next(error)
-    }
-  }
-  async deleteTask(req, res, next) {
-    try {
-      const task = await tasksService.deleteTask(req.params.taskId, req.userInfo.id)
-      res.send(task)
-    } catch (error) {
-      next(error)
-    }
-  }
-  async editTask(req, res, next) {
-    try {
-      const task = await tasksService.editTask(req.params.taskId, req.userInfo.id, req.body)
-      res.send(task)
-    } catch (error) {
-      next(error)
-    }
-  }
-
-
 
 }
