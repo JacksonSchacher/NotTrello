943410cdba6a4f7d54159a577781f5f2890b98fc
Hannah Milam <hannah.milam16@gmail.com>
2021-09-28 13:49:16 -0600

    formatting home page

--- NotTrello.client/src/components/Projects.vue
+++ NotTrello.client/src/components/Projects.vue
@@ -1,11 +1,13 @@
 <template>
-<div class="row">
-<div class="col-md-6">
-<p>{{ project.name }}</p>
-<div class="col-md-6">
-<p>{{ project.description }}</p>
-</div>
-</div>
+  <div class="card-body">
+    <div class="row justify-content-between mx-5">
+      <div class="col-2">
+    <p>{{ project.name }}</p>
+      </div>
+      <div class="col-2 offset-4">
+    <p>{{ new Date(project.createdAt).toDateString() }}</p>
+      </div>
+    </div>
 </div>
 </template>
 
