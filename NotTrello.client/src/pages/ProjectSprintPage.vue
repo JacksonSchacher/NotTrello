1ad19374144946a91202d0d5e56ac72e0bb46f51
Hannah Milam <hannah.milam16@gmail.com>
2021-09-28 15:18:04 -0600

    added router-view on parent page

---
+++ NotTrello.client/src/pages/ProjectSprintPage.vue
@@ -0,0 +1,19 @@
+<template>
+ <div class="container-fluid">
+   <div class="row">
+     <div class="col-6">
+       <h1>TESTING</h1>
+     </div>
+   </div>
+ </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
