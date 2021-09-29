ab91872810e98c2bc9c898c4dabc25c7c348f5e8
Jackson Schacher <jackson.schacher@gmail.com>
2021-09-29 15:11:18 -0600

    got delete backlogs to work

--- NotTrello.client/src/services/SprintsService.js
+++ NotTrello.client/src/services/SprintsService.js
@@ -1,36 +1,37 @@
-import { AppState } from "../AppState"
-import { Sprint } from "../models/Sprint"
-import { logger } from "../utils/Logger"
-import { api } from "./AxiosService"
+import { AppState } from '../AppState'
+import { Sprint } from '../models/Sprint'
+import { logger } from '../utils/Logger'
+import { api } from './AxiosService'
 
-class SprintsService{
+class SprintsService {
+  async getSprints(projectId) {
+    const res = await api.get(`api/projects/${projectId}/sprints`)
+    logger.log('getSprints', res)
+    AppState.sprints = res.data.map(s => new Sprint(s))
+  }
 
-async getSprints(projectId){
-  const res = await api.get(`api/projects/${projectId}/sprints`)
-  logger.log('getSprints', res)
-  AppState.sprints = res.data.map(s => new Sprint(s))
-}
+  async getSprintsById(projectId, sprintId) {
+    AppState.sprints = null
+    const res = await api.get(`api/projects/${projectId}/sprints/${sprintId}`)
+    AppState.sprints = new Sprint(res.data)
+  }
 
-async getSprintsById(projectId, sprintId){
-  AppState.sprints = null
-  const res = await api.get(`api/projects/${projectId}/sprints/${sprintId}`)
-  AppState.sprints = new Sprint(res.data)
-}
-async createSprint(projectId, sprint){
-const res = await api.post(`api/projects/${projectId}/sprints`, sprint)
-AppState.sprints = [...AppState.sprints, new Sprint((res.data))]
-logger.log('create sprints', res)
-}
-async editSprint(projectId, sprint){
-  const res = await api.put(`api/projects/${projectId}/sprints/${sprint.id}`, sprint)
-  AppState.sprints = new Sprint(res.data)
-  logger.log('edit sprints', res)
-}
+  async createSprint(projectId, sprint) {
+    const res = await api.post(`api/projects/${projectId}/sprints`, sprint)
+    AppState.sprints = [...AppState.sprints, new Sprint((res.data))]
+    logger.log('create sprints', res)
+  }
 
-async deleteSprint(projectId, sprintId) {
-  const res = await api.delete(`api/projects/${projectId}/sprints/${sprint.id}`)
-  AppState.sprints = null
-  AppState.sprints.filter(s => s.id !== sprintId)
-}
+  async editSprint(projectId, sprint) {
+    const res = await api.put(`api/projects/${projectId}/sprints/${sprint.id}`, sprint)
+    AppState.sprints = new Sprint(res.data)
+    logger.log('edit sprints', res)
+  }
+
+  async deleteSprint(projectId, sprintId) {
+    const res = await api.delete(`api/projects/${projectId}/sprints/${sprint.id}`)
+    AppState.sprints = null
+    AppState.sprints.filter(s => s.id !== sprintId)
+  }
 }
 export const sprintsService = new SprintsService()
