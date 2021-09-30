665084b21e91fead2201c950d04d92eaa6fc0354
Hannah Milam <hannah.milam16@gmail.com>
2021-09-30 11:59:59 -0600

    working on navbar

--- NotTrello.client/src/pages/ProjectPage.vue
+++ NotTrello.client/src/pages/ProjectPage.vue
@@ -1,21 +1,30 @@
 <template>
   <div class="container-fluid mb-3">
-    <div class="row py-2 mb-4 bg-dark">
-      <ul class="nav-links mb-0 d-flex align-items-center justify-content-around">
-        <router-link :to="{name:'Project.Backlog'}">
+    <div class="row pt-2 top-bar" style="background-color: rgba(0, 0, 0, 0.616);">
+      <ul class="nav nav-links d-flex align-items-center justify-content-around">
+         <p class="text-light mt-2">
+      <b>
+      {{ currentProject.name }}
+      </b>
+      <i class="mdi mdi-delete f-15 selectable ms-2" @click="deleteProject(currentProject.id)"></i>
+      </p>
+
+          
+        <router-link :to="{name:'Project.Backlog'}" exact-active-class="IS-ACTIVATED">
           <li class="nav-link">
             Backlog
           </li>
         </router-link>
-        <router-link :to="{name:'Project.Sprint'}">
-          <li class="nav-link">
-            Sprint
-          </li>
+          
+
+          <div v-for="s in sprints" :key="s.id" :sprint="s" >
+        <router-link :to="{name:'Project.Sprint'}" exact-active-class="IS-ACTIVATED">
+           <li class="nav-link">
+             {{ s.name }}
+           </li>
         </router-link>
-        <li class="nav-link">
-      {{ currentProject.name }}
-      <i class="mdi mdi-delete f-15 selectable ms-2" @click="deleteProject(currentProject.id)"></i>
-      </li>
+          </div>
+
       </ul>
     </div>
   </div>
@@ -23,20 +32,32 @@
 </template>
 
 <script>
-import { computed } from '@vue/runtime-core'
+import { computed, onMounted } from '@vue/runtime-core'
 import { AppState } from '../AppState'
 import { logger } from '../utils/Logger'
 import Pop from '../utils/Pop'
 import { projectsService } from '../services/ProjectsService'
 import { router } from '../router'
+import { useRoute } from 'vue-router'
+import { sprintsService } from '../services/SprintsService'
 
 export default {
-  name: 'Project',
   setup() {
+    const route = useRoute()
+    onMounted(async () => {
+      try {
+        await sprintsService.getSprints(route.params.id)
+        await projectsService.getProjectById(route.params.id)
+      } catch (error) {
+        Pop.toast(error.message, 'error')
+        logger.log('getSprints', error.message)
+      }
+    })
     return {
       account: computed(() => AppState.account),
       project: computed(() => AppState.project),
       backlog: computed(() => AppState.backlogs),
+      sprints: computed(() => AppState.sprints),
       currentProject: computed(() => AppState.currentProject),
        async deleteProject(currentProjectId) {
         try {
@@ -54,6 +75,14 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+.nav-link{
+  text-transform: uppercase;
+}
+ .navbar-nav .router-link-exact-active{
+   color: pink;
+ }
+ .hoverable {
+  cursor: pointer;
+}
 </style>
